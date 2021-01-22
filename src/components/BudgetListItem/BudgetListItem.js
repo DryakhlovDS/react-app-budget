a0b517aae28d3f83c5321fa18a257472316e9ec7
DryakhlovDS <dryakhlovds@gmail.com>
2021-01-22 21:49:45 +0300

    add icons and color in BudgetListItem

--- src/components/BudgetListItem/BudgetListItem.js
+++ src/components/BudgetListItem/BudgetListItem.js
@@ -3,11 +3,13 @@ import { Button } from 'element-react';
 import './BudgetListItem.css';
 
 function BudgetListItem({ item }) {
-  console.log(item);
+  const icon = item.type === 'Income' ? 'el-icon-plus' : 'el-icon-minus';
+  const colorText = item.type === 'Income' ? 'green' : 'red';
   return (
     <div className='list-item'>
+      <i className={[icon, colorText].join(' ')}></i>
       <span className='budget-comment'>{item.comment}</span>
-      <span className='budget-value'>{item.cash}</span>
+      <span className={['budget-value', colorText].join(' ')}>{item.cash}</span>
       <Button type='danger' size='small' plain='true'>
         Delete
       </Button>
--- src/components/TotalBalance/TotalBalance.js
+++ src/components/TotalBalance/TotalBalance.js
@@ -3,7 +3,12 @@ import './TotalBalance.css';
 
 function TotalBalance({ list }) {
   const total = list.reduce((acc, item) => acc + item.cash, 0);
-  return <div className='total-value'>Total balance : {total}</div>;
+  const colorText = total > 0 ? 'good-balance' : total < 0 ? 'bad-balance' : '';
+  return (
+    <div className={['total-value', colorText].join(' ')}>
+      Total balance : {total}
+    </div>
+  );
 }
 
 export default TotalBalance;
